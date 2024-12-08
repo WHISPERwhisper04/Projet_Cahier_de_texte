8cee181fc09f4f236cc1675d1b3fe4ec90a99137
WHISPERwhisper04 <161390129+WHISPERwhisper04@users.noreply.github.com>
2024-12-09 01:13:48 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -9,33 +9,43 @@ public function __construct(){
     $this->conn = mysqli_connect($this->servername, $this->username,$this->password);
     // Check connection
     if (!$this->conn) {
-        die("Connection failed: " .mysqli_connect_error());}
-    else {
-       //echo "Connected successfully";
-        // echo "Error creating database: " .
-        // mysqli_error($this->conn);
+        die("Connection failed: " .mysqli_connect_error());
         }
 }
-function createDatabase($dbName){
-    $sql = "CREATE DATABASE " . $dbName;  
-    if (mysqli_query($this->conn, $sql)) {  
-        echo "Database created successfully";  
-    } else {  
-        echo "Error creating database: " . mysqli_error($this->conn);  
-    }  
+// function createDatabase($dbName){
+//     $sql = "CREATE DATABASE " . $dbName;  
+//     if (mysqli_query($this->conn, $sql)) {  
+//         echo "Database created successfully";  
+//     } else {  
+//         echo "Error creating database: " . mysqli_error($this->conn);  
+//     }  
     
+// }
+public function createDatabase($dbName){
+    $result = mysqli_query($this->conn, "SHOW DATABASES LIKE '$dbName'");
+    if (mysqli_num_rows($result) == 0) {
+        $sql = "CREATE DATABASE " . $dbName;
+        if (mysqli_query($this->conn, $sql)) {
+            echo "Database created successfully";
+        } else {
+            echo "Error creating database: " . mysqli_error($this->conn);
+        }
+    } else {
+     //   echo "Database '$dbName' already exists.";
+    }
 }
-function selectDatabase($dbName){
+
+public function selectDatabase($dbName){
     //select database with the conn of the class, using mysqli_select..
     mysqli_select_db($this->conn,$dbName);
 }
 
-function createTable($query){
+public function createTable($query){
   //creating a table with the query
   if (mysqli_query($this->conn, $query)) {
         echo "Table created successfully";
     } else {
-        echo "Error creating table: " . mysqli_error($this->conn);
+      //  echo "Error creating table: " . mysqli_error($this->conn);
     }
     
 }
